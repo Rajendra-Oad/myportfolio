ec2e3f37cac5f4dc2a5785780614bf630e8bd499
Rajendra-Oad <154015159+Rajendra-Oad@users.noreply.github.com>
2024-12-26 17:09:25 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,26 @@ const scroll = new LocomotiveScroll({
 
   smooth:true
 });
+
+
+// toggle dark mode
+
+let darkMode = localStorage.getItem('dark_mode');
+const darkToggle = document.getElementById('theme_switch')
+
+darkToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('dark_mode');
+  darkMode !== 'active' ? enableDarkMode() : disableDarkMode();
+})
+
+const enableDarkMode = () => {
+  document.body.classList.add('dark_mode');
+  localStorage.setItem('dark_mode','active');
+}
+
+const disableDarkMode = () =>{
+  document.body.classList.remove('dark_mode');
+  localStorage.setItem('dark_mode',null);
+}
+
+if (darkMode === 'active') enableDarkMode()
